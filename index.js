dbc86ef706d12faffa310cf2ac3d13c10a3ef58a
Agust Rinaldo Kurniawan <agust.kurniawan@student.bakrie.ac.id>
2020-07-30 22:46:06 +0700

    initial

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 app.use(compression());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
